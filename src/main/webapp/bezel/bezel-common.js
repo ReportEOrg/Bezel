447bfb1cea3019126b10a4c8c9baaca9e2af2475
wongmk <MinKeat.Wong@centurylink.com>
2015-07-08 11:06:25 +0800

    added displaying of error message for not success ajax call + display proper empty row table

--- src/main/webapp/bezel/bezel-common.js
+++ src/main/webapp/bezel/bezel-common.js
@@ -24,3 +24,25 @@ $(document).ready(function(){
 function getRestServerUrl(){
 	return "http://localhost:8080";
 }
+
+function displayAjaxError(){
+	var spanElement = document.createElement("span");
+	$(spanElement).text("Failed to retrieve data from remote server");
+	$('#ajaxErrorMsg').append(spanElement);
+	
+	var buttonElement = document.createElement("button");
+	$(buttonElement).attr({type: "button",
+						   class: "close"});
+
+	$(buttonElement).html("&times;");
+	
+	$(buttonElement).click(function(){
+		$('#ajaxErrorMsg').hide();
+		$('#ajaxErrorMsg').empty();
+	});
+	
+	$('#ajaxErrorMsg').append(buttonElement);
+	
+	$('#ajaxErrorMsg').show();
+	
+}
